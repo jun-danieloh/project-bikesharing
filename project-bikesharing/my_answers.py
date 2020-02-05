4bac36f88c7e30102fb5df33a58cf9ac345a061f
jun1-oh <jun1.oh@samsung.com>
2020-02-05 11:58:36 +0900

    Feb 5

--- project-bikesharing/my_answers.py
+++ project-bikesharing/my_answers.py
@@ -144,7 +144,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 100
-learning_rate = 0.1
-hidden_nodes = 2
+iterations = 7000
+learning_rate = 0.8
+hidden_nodes = 10
 output_nodes = 1
